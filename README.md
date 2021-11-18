9fc7411257af18f1a56a1092481a675b5be70186
crishard <crislantorres9@gmail.com>
2021-11-18 18:08:13 -0300

    adicionando comentarios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Criando gráficos com python utilizando a biblioteca matplotlib do python
+
+Antes de mais nada é preciso instalar o pacote da biblioteca, basta utilizar o comando pip install matplotlib
--- grafico.py
+++ grafico.py
@@ -1,14 +1,25 @@
+#importando biblioteca e colocando o comando "grf" como comando da bibioteca
 import matplotlib.pyplot as grf
 
+#definindo título para o gráfico
 grf.title('Faturaamento em 2021')
+
+#informações eixo x
 grf.xlabel('Meses')
+
+#informações eixo y
 grf.ylabel('Faturamento em R$')
+
+#criando as listas de dados
 meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 
 valores = [520, 245, 322, 587, 360, 142, 189, 680, 470, 155, 169, 520]
 
+#escolhendo o gráfico
 """"grf.scatter para grafico de pontos
     grf.plot para grafico de linhas"""
 grf.bar(meses, valores)
+
+#chamando função para apresentar o gráfico
 grf.show()
 
