a47aea3bbd58e4701cca74d8e2eb3cb03311a509
crishard <crislantorres9@gmail.com>
2021-11-23 15:47:13 -0300

    Adicionando títulos e informaões de eixo

--- figure with graphics/figure.py
+++ figure with graphics/figure.py
@@ -3,20 +3,30 @@ import matplotlib.pyplot as plt
 #informações para o gráfico um da figura
 name = ['José', 'Francisco', 'Maria']
 age = [32, 25, 34]
+plt.title('Gráfico de barras')
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo Y')
 
 #informações pro gráfico dois da figura
 brand = ['Ferrrari', 'Audi', 'Chevrolet']
 sales = [9, 14, 11]
+plt.title('Gráfico de pontos')
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo Y')
 
 #informações pro gráfico três da figura
 product = ['Sugar', 'Coffee', 'Rice']
 cost = [3.43, 2.22, 4.15]
+plt.title('Gráfico de pontos vermelhos')
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo Y')
 
 #informações para o gráfico quatro da figura
 people = ['Roberta', 'Juarez', 'Geogirna']
 bmi = [23.5, 32.2, 18.37]
-
-plt.figure()
+plt.title('Gráfico de linhas')
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo Y')
 
 plt.subplot(221)
 plt.bar(name, age)
@@ -30,5 +40,6 @@ plt.plot(product, cost, 'ro')
 plt.subplot(224)
 plt.plot(people, bmi)
 
+plt.suptitle('Titulo da figura')
 plt.savefig('figure.jpeg')
 plt.show()
